5473ce91bf5ad76793ead6b1dd886dccf09b6aca
Mattis Ericsson Bergman <mattisodin@gmail.com>
2025-01-10 11:19:56 +0100

    Interaction logic for buttons added

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ToDoList
 {
@@ -16,26 +7,51 @@ namespace ToDoList
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private ToDoListLogic _ToDoListLogic;
         public MainWindow()
         {
             InitializeComponent();
+            _ToDoListLogic = new ToDoListLogic();
         }
+
+
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             string task = TaskTextBox.Text;
+
+            if (!string.IsNullOrEmpty(task))
+            {
+                _ToDoListLogic.AddTask(task);
+                UpdateTaskList();
+                TaskTextBox.Clear();
+            }
+
+
         }
+        //adds the task and clears the textbox
+
+        private void UpdateTaskList()
+        {
+            TasksListBox.Items.Clear();
+            foreach (var task in _ToDoListLogic.GetAllTasks())
+            {
+                TasksListBox.Items.Add(task);
+            }
+        }
+        //Updates the list of tasks by clearing out the old list and updating it with the new items
 
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
+            if (TasksListBox.SelectedIndex >= 0)
+            {
+                _ToDoListLogic.RemoveTask(TasksListBox.SelectedIndex);
+                UpdateTaskList();
 
+            }
         }
-
-        private void TasksListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
+        //removes the task at the selected index and then updates the list
 
     }
 }
--- ToDoList/ToDoListLogic.cs
+++ ToDoList/ToDoListLogic.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToDoList
+﻿namespace ToDoList
 {
 
     //<summary>
@@ -25,7 +19,7 @@ namespace ToDoList
             {
                 tasks.RemoveAt(index);
             }
-            
+
         }
 
         public List<string> GetAllTasks()
