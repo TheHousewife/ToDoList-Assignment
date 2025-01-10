e3497998c78ab251e614fd3fa39c420efb7adacc
Mattis Ericsson Bergman <mattisodin@gmail.com>
2025-01-10 10:16:40 +0100

    basic task logic added

--- ToDoList/ToDoListLogic.cs
+++ ToDoList/ToDoListLogic.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace ToDoList
 {
-    internal class ToDoListLogic
+
+    //<summary>
+    // Handles logic for adding, removing and showing todo items
+    //</summary>
+    public class ToDoListLogic
     {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+
+        public void RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+            
+        }
+
+        public List<string> GetAllTasks()
+        {
+            return tasks;
+        }
     }
 }
